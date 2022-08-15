f66e7ba8a8b618833225648ba304486d09e9c06e
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2022-08-15 12:33:09 +0200

    new commit

---
+++ src/main/java/com/banquemisr/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.banquemisr.repository;
+
+import com.banquemisr.entity.app_user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<app_user,Long>  {
+    app_user findByName(String name);
+}
--- src/main/java/com/banquemisr/security/ApplicationSecurityConfig.java
+++ src/main/java/com/banquemisr/security/ApplicationSecurityConfig.java
@@ -1,52 +1,50 @@
-package com.banquemisr.security;
-/*
-import com.banquemisr.filter.CustomAuthenticationFilter;
-import com.banquemisr.filter.CustomAuthorizationFiler;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    UserDetailsService userDetailsService;
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter customAuthFilter=new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthFilter.setFilterProcessesUrl("/api/login");
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/api/login").permitAll();
-        http.authorizeRequests().antMatchers(GET,"api/user/**").hasAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST,"api/user/save**").hasAuthority("ROLE_ADMIN");
-        http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(customAuthFilter);
-        http.addFilterBefore(new CustomAuthorizationFiler(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception
-    {
-        return super.authenticationManagerBean();
-    }
-}
-
-*/
+//package com.banquemisr.security;
+//
+//import com.banquemisr.filter.CustomAuthenticationFilter;
+//import com.banquemisr.filter.CustomAuthorizationFiler;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import static org.springframework.http.HttpMethod.GET;
+//import static org.springframework.http.HttpMethod.POST;
+//import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    UserDetailsService userDetailsService;
+//    @Autowired
+//    BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        CustomAuthenticationFilter customAuthFilter=new CustomAuthenticationFilter(authenticationManagerBean());
+//        customAuthFilter.setFilterProcessesUrl("/api/login");
+//        http.csrf().disable();
+//        http.sessionManagement().sessionCreationPolicy(STATELESS);
+//        http.authorizeRequests().antMatchers("/api/login").permitAll();
+//        http.authorizeRequests().antMatchers(GET,"api/user/**").hasAuthority("ROLE_USER");
+//        http.authorizeRequests().antMatchers(POST,"api/user/save**").hasAuthority("ROLE_ADMIN");
+//        http.authorizeRequests().anyRequest().authenticated();
+//        http.addFilter(customAuthFilter);
+//        http.addFilterBefore(new CustomAuthorizationFiler(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception
+//    {
+//        return super.authenticationManagerBean();
+//    }
+//}
