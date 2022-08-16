18d9eb571c79a9392dccb955e8ba85b658d01afe
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2022-08-16 16:24:28 +0200

    new fix

--- src/main/java/com/banquemisr/controllers/roleController.java
+++ src/main/java/com/banquemisr/controllers/roleController.java
@@ -1,41 +1,41 @@
-package com.banquemisr.controllers;
-
-import com.banquemisr.entity.Role;
-import com.banquemisr.service.RoleImplService;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-
-@RestController
-@RequestMapping("role")
-public class roleController {
-    @Autowired
-    RoleImplService roleService;
-    @PreAuthorize("permitAll()")
-    @PostMapping("/add")
-    public ResponseEntity<Role> addRole(@RequestBody Role role)
-    {
-        URI uri= URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
-        return ResponseEntity.created(uri).body(roleService.saveRole(role));
-    }
-    @PreAuthorize("permitAll()")
-    @PostMapping("/assignToUser")
-    public void assignRoleToUser(@RequestBody RoleToUser roleToUser)
-    {
-        roleService.addRoleToUser(roleToUser.getUsername(),roleToUser.getRolename());
-    }
-}
-@Data
-class RoleToUser
-{
-    private String username;
-    private String rolename;
-}
+//package com.banquemisr.controllers;
+//
+//import com.banquemisr.entity.Role;
+//import com.banquemisr.service.RoleImplService;
+//import lombok.Data;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//
+//import java.net.URI;
+//
+//@RestController
+//@RequestMapping("role")
+//public class roleController {
+//    @Autowired
+//    RoleImplService roleService;
+//    @PreAuthorize("permitAll()")
+//    @PostMapping("/add")
+//    public ResponseEntity<Role> addRole(@RequestBody Role role)
+//    {
+//        URI uri= URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+//        return ResponseEntity.created(uri).body(roleService.saveRole(role));
+//    }
+//    @PreAuthorize("permitAll()")
+//    @PostMapping("/assignToUser")
+//    public void assignRoleToUser(@RequestBody RoleToUser roleToUser)
+//    {
+//        roleService.addRoleToUser(roleToUser.getUsername(),roleToUser.getRolename());
+//    }
+//}
+//@Data
+//class RoleToUser
+//{
+//    private String username;
+//    private String rolename;
+//}
--- src/main/java/com/banquemisr/controllers/userController.java
+++ src/main/java/com/banquemisr/controllers/userController.java
@@ -13,7 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("user")
 public class userController {
