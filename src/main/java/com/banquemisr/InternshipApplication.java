088ca9828615cd8890e254c4ef34588f7829460f
Shady Akl <sakl@afaqdesigns.com>
2022-08-18 01:30:49 +0200

    configure CORS ORIGIN

--- src/main/java/com/banquemisr/InternshipApplication.java
+++ src/main/java/com/banquemisr/InternshipApplication.java
@@ -10,16 +10,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class InternshipApplication {
 
     public static void main(String... args) {
         SpringApplication.run(InternshipApplication.class, args);
     }
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
@@ -31,7 +35,7 @@ public class InternshipApplication {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
         config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
         config.addAllowedMethod("GET");
